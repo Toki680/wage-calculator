d07ba46c2230bc09136e6d301ed551a114f42133
Toki680 <jyzhou1079@gmail.com>
2025-06-12 17:04:25 -0400

    clean up script.js

--- script.js
+++ script.js
@@ -2,33 +2,26 @@ function calc() {
   // number of weekdays
   const weekdays = new Array(7).fill(0);
   // user inputs of dates
-  const begin_date = new Date(document.getElementById("begin_date").value);
-  const end_date = new Date(document.getElementById("end_date").value);
+  const begin_date = new Date(document.getElementById('begin_date').value);
+  const end_date = new Date(document.getElementById('end_date').value);
   const weekday_names = [
-    "Monday // 周一",
-    "Tuesday // 周二",
-    "Wednesday // 周三",
-    "Thursday // 周四",
-    "Friday // 周五",
-    "Saturday // 周六",
-    "Sunday // 周日",
+    'Monday // 周一',
+    'Tuesday // 周二',
+    'Wednesday // 周三',
+    'Thursday // 周四',
+    'Friday // 周五',
+    'Saturday // 周六',
+    'Sunday // 周日',
   ];
   // user inputs of hours each day
-  const sunday_hr = document.getElementById("sunday_hr").value;
-  const monday_hr = document.getElementById("monday_hr").value;
-  const tuesday_hr = document.getElementById("tuesday_hr").value;
-  const wednesday_hr = document.getElementById("wednesday_hr").value;
-  const thursday_hr = document.getElementById("thursday_hr").value;
-  const friday_hr = document.getElementById("friday_hr").value;
-  const saturday_hr = document.getElementById("saturday_hr").value;
   const weekday_hrs = [
-    monday_hr,
-    tuesday_hr,
-    wednesday_hr,
-    thursday_hr,
-    friday_hr,
-    saturday_hr,
-    sunday_hr,
+    document.getElementById('monday_hr').value,
+    document.getElementById('tuesday_hr').value,
+    document.getElementById('wednesday_hr').value,
+    document.getElementById('thursday_hr').value,
+    document.getElementById('friday_hr').value,
+    document.getElementById('saturday_hr').value,
+    document.getElementById('sunday_hr').value,
   ];
   
   // parse through each day and count weekdays
@@ -37,11 +30,11 @@ function calc() {
   }
   
   // display number of weekdays
-  let result_text = "";
+  let result_text = '';
   weekdays.forEach((count, index) => {
     result_text += `${weekday_names[index]}: ${count}<br>`;
   });
-  document.getElementById("detail").innerHTML = result_text;
+  document.getElementById('detail').innerHTML = result_text;
 
   // display pay period
   if (!isNaN(begin_date)) {
@@ -56,7 +49,7 @@ function calc() {
     var end = displayEnd.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
   }
 
-  document.getElementById("pay_period").innerHTML = "Pay Period // 工资周期: " + begin + " - " + end;
+  document.getElementById('pay_period').innerHTML = 'Pay Period // 工资周期: ' + begin + ' - ' + end;
 
   // calculate total hours; number of weekday * hour of weekday
   let total_hrs = 0;
@@ -64,19 +57,19 @@ function calc() {
     let result = weekdays[i] * weekday_hrs[i];
     total_hrs += result;
   }
-  document.getElementById("hours").innerHTML = "Total Hours // 总工时: " + total_hrs;
+  document.getElementById('hours').innerHTML = 'Total Hours // 总工时: ' + total_hrs;
 
   // calculate total wages; total hours * wage
-  var total_wages = total_hrs * document.getElementById("wage").value;
-  document.getElementById("wages").innerHTML = "Total Wages // 总工资: " + total_wages;
+  var total_wages = total_hrs * document.getElementById('wage').value;
+  document.getElementById('wages').innerHTML = 'Total Wages // 总工资: ' + total_wages;
 }
 
 // limit hour input
 function limit(input) {
+  const warning = document.getElementById("warning");
   if (input.value > 24 || input.value < 0) {
-    document.getElementById("warning").style.opacity = 100;
-  }
-  else if (input.value < 25 && input.value >= 0 ) {
-    document.getElementById("warning").style.opacity = 0;
+    warning.style.opacity = 1;
+  } else {
+    warning.style.opacity = 0;
   }
 }
